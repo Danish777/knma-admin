0f32aac7b15e0a8900a47dec5eb8dfd12422feeb
Mohd Danish <erdanish2008@gmail.com>
2024-02-29 11:06:18 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// scroll bar
+import 'simplebar/src/simplebar.css';
+// apex-chart
+import 'assets/third-party/apex-chart.css';
+
+import { StrictMode } from 'react';
+
+import { createRoot } from 'react-dom/client';
+// third-party
+import { Provider as ReduxProvider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from 'store';
+
+// project import
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// ==============================|| MAIN - REACT DOM RENDER  ||============================== //
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <StrictMode>
+    <ReduxProvider store={store}>
+      <BrowserRouter basename="/">
+        <App />
+      </BrowserRouter>
+    </ReduxProvider>
+  </StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
